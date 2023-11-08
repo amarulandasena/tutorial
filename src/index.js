fad5ad1a99b7d4d719010ff6ab8a6185a48014da
Andres_Marulanda <afmsamsung1981@gmail.com>
2023-11-08 11:22:00 -0500

    inicio tutorial js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const fn = () {
+    const miString = "!Soy un String muy feliz"
+    let b = miString + "en esta vida."
+    console.log(b)
+}
+
+fn()
+
+const clickeado = () => {
+    let a = "Amo la programción."
+
+    console.log(a)
+}
